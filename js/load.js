8bda153c8554ba26f404709c66b93ca40ebb7d0a
romodsite <romodsite@gmail.com>
2024-12-05 08:11:26 +0100

    Add files via upload

---
+++ js/load.js
@@ -0,0 +1,365 @@
+const content = document.getElementById("content");
+const button = document.getElementById("loadMoreButton");
+let startIndex = 0;
+let bddToLoad = "home";
+const input = document.getElementById('searchInput');
+const divInput = document.getElementById('input');
+var canExecute = true;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Si bddToLoad != "cash" => charger les éléments avec un style classique
+function loadBddElements(data) {
+    // Ajouter les nouveaux éléments au contenu
+    data.forEach((item) => {
+        const divContentBox = document.createElement("div");
+        divContentBox.classList.add("content-box");
+        
+        const img = document.createElement("img");
+        img.src = item.image;
+        img.alt = item.name
+        img.fetchPriority = "high";
+        img.loading = "lazy";
+        img.classList.add("content-img");
+
+        const h3 = document.createElement("h3");
+        h3.textContent = item.name;
+        h3.classList.add("skiptranslate");
+
+        const a = document.createElement("a");
+        a.href = item.link;
+        a.target = "_blank";
+        a.textContent = "Accéder au Mega";
+        a.classList.add("linkvertise");
+
+        const divForTimeAndName = document.createElement("div");
+        divForTimeAndName.classList.add("timeAndName");
+        var time;
+
+        if (bddToLoad === 'leak') {
+
+            time = document.createElement("p");
+            time.textContent = item.datepost;
+            time.classList.add("time");
+
+            // const report = document.createElement('div');
+            // report.classList.add("reportFlag");
+            // report.setAttribute('onclick', 'reportEvent(this)')
+
+            // const imgReport = document.createElement('img')
+            // imgReport.src = "img/flags.png";
+            
+            // report.appendChild(imgReport)
+            // divContentBox.appendChild(report)
+        }
+
+        divContentBox.appendChild(img);
+        
+        if (bddToLoad === 'leak') {
+            divForTimeAndName.appendChild(h3);
+            divForTimeAndName.appendChild(time);
+            divForTimeAndName.appendChild(a);
+            divContentBox.appendChild(divForTimeAndName);
+        } else {
+            
+            divForTimeAndName.appendChild(h3);
+            divForTimeAndName.appendChild(a);
+            divContentBox.appendChild(divForTimeAndName);
+        }
+
+        content.appendChild(divContentBox);
+    });
+}
+
+// Si bddToLoad == "cash" => charger les éléments avec le style de cash
+function loadCashElements(data) {
+    console.log("ok")
+    console.log(data)
+    data.forEach((item) => {
+
+        if (item.BigTitle) {
+            const newTitle = document.createElement("h2");
+            newTitle.textContent = item.BigTitle;
+            newTitle.style.width = "100%";
+            newTitle.style.textAlign = "center";
+            newTitle.style.fontSize = "30px";
+            
+            content.appendChild(newTitle);
+            return
+        }
+
+        // Créer la div principale
+        const contentDiv = document.createElement('div');
+        contentDiv.classList.add('content-earn-money');
+
+        // Créer et ajouter l'image
+        const image = document.createElement('img');
+        image.classList.add('content-img-earn-money');
+        image.src = item.image;
+        image.alt = '';
+        contentDiv.appendChild(image);
+
+        // Créer et ajouter le titre (GetGrass)
+        const title = document.createElement('h3');
+        title.classList.add('content-width', 'skiptranslate');
+        title.textContent = item.title;
+        contentDiv.appendChild(title);
+
+        // Créer et ajouter la description
+        const description = document.createElement('p');
+        description.classList.add('content-width');
+        description.textContent = item.description;
+        contentDiv.appendChild(description);
+
+        // Créer et ajouter le titre "Instruction:"
+        const instructionsTitle = document.createElement('h3');
+        instructionsTitle.classList.add('content-width');
+        instructionsTitle.textContent = 'Instruction:';
+        contentDiv.appendChild(instructionsTitle);
+
+        // Créer et ajouter la liste d'instructions
+        const instructionsList = document.createElement('ol');
+        instructionsList.classList.add('content-width-ol');
+        item.instructions.forEach(instruction => {
+            const listItem = document.createElement('li');
+            listItem.classList.add('content-width');
+            listItem.textContent = instruction;
+            instructionsList.appendChild(listItem);
+        });
+        contentDiv.appendChild(instructionsList);
+
+        // Créer et ajouter le lien
+        const link = document.createElement('a');
+        link.classList.add('content-access-earn-money');
+        link.href = item.link;
+        link.textContent = "S'inscrire";
+        contentDiv.appendChild(link);
+
+        // Ajouter la div au body
+        content.appendChild(contentDiv);
+    })
+}
+
+// Charger les éléments de Home
+function loadHome(toLoad) {
+    var xhr = new XMLHttpRequest();
+    if (toLoad === 'home') {
+        loadHTML = 'info.html';
+    } else if (toLoad === 'donate') {
+        loadHTML = 'donation.html';
+    }
+    xhr.open('GET', loadHTML, true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            content.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
+// Charger les données au démarrage
+function start() {
+    if (bddToLoad == 'home' || bddToLoad == 'donate') {
+        divInput.style.display = 'none';
+        button.style.display = 'none';
+        content.style.minHeight = '95%';
+        loadHome(bddToLoad);
+        return
+    }
+
+    divInput.style.display = 'flex';
+    button.style.display = 'flex';
+    content.style.minHeight = '';
+
+    if (bddToLoad == 'cash') {
+        divInput.style.display = 'none';
+        button.style.display = 'none';
+    }
+
+    var urlToLoad;
+    var offsetIndex = 40;
+
+    if (bddToLoad == 'cash') {
+        urlToLoad = `./testDatabaseCash.json`
+    } else if (bddToLoad == 'leak') {
+        urlToLoad = `https://api.megafree.xyz/items/${startIndex}/${offsetIndex}`
+    }
+
+    console.log(urlToLoad)
+    
+    // fetch(`http://localhost:5000/load-leak-data/${startIndex}/${bddToLoad}`)
+    fetch(`${urlToLoad}`)
+        .then((response) => response.json())
+        .then((responseData) => {
+            console.log(responseData.length)
+            const data = responseData;
+            console.log(data)
+            startIndex += 40;
+
+            if (responseData.length < 40 || responseData.length == 0) {
+                document.getElementById("loadMoreButton").style.display = "none";
+            }
+
+            if (data && bddToLoad != "cash") {
+                // charger les éléments
+                loadBddElements(data);
+            } else if (data && bddToLoad == "cash") {
+                // charger les éléments
+                loadCashElements(data);
+            } else {
+                content.innerHTML = ""
+                const div = document.createElement("h2");
+                div.textContent = "Error 400 Bad Request";
+                content.appendChild(div);
+                console.error("Bad response from server");
+            }
+            
+            // Désactiver le bouton si `nextStartIndex` est null
+            // if (startIndex === null) {
+            //     document.getElementById("loadMoreButton").style.display = "none";
+            // }
+        })
+        .catch((error) => console.error("Erreur:", error));
+}
+
+// Résultat de la recherche
+
+function search() {
+    const query = document.getElementById("searchInput").value.trim();
+
+    if (query) {
+        fetch(`https://api.megafree.xyz/items/${query}`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                content.innerHTML = ""; // Effacer les anciens résultats
+                button.style.display = "none";
+
+                if (data && data.length > 0 && bddToLoad != "cash") {
+                    const message = document.createElement("h2");
+                    message.textContent =
+                        "Voici les résultat les plus proches de votre recherche";
+                    message.style.width = "100%";
+                    message.style.textAlign = "center";
+                    content.appendChild(message);
+
+                    // Afficher les résultats partiels triés par pertinence
+                    loadBddElements(data);
+                } else if (data && bddToLoad == "cash") {
+                    loadCashElements(data);
+                    console.log(data);
+                } else {
+                    // Afficher un message si aucun résultat n'est trouvé
+                    const div = document.createElement("h2");
+                    div.textContent = "Aucun résultat trouvé";
+                    content.appendChild(div);
+                }
+            })
+            .catch((error) =>
+                console.error("Erreur lors de la recherche:", error)
+            );
+    } else {
+        // Si la barre de recherche est vide
+        content.innerHTML = "";
+        startIndex = 0;
+        button.style.display = "flex";
+        start();
+    }
+}
+
+async function canExecuteWait() {
+    canExecute = false;
+    await sleep(1000);
+    canExecute = true;
+}
+
+// load more button
+document.getElementById("loadMoreButton").addEventListener("click", () => {
+    if (canExecute) {
+        canExecuteWait();
+        start();
+    } else {
+        alert("Wait 1 seconde before next requests");
+    }
+});
+
+// search button
+document.getElementById("searchButton").addEventListener("click", () => {
+    if (canExecute) {
+        canExecuteWait();
+        search();
+    } else {
+        alert("Wait 1 seconde before next requests");
+    }
+});
+
+
+// Ajouter un événement 'keypress' sur l'input
+input.addEventListener('keypress', function(event) {
+    if (canExecute) {
+        if (event.key === 'Enter') { // Vérifie si la touche appuyée est 'Enter'
+            canExecuteWait();
+            event.preventDefault(); // Empêche le comportement par défaut si nécessaire
+            search();
+        }
+    } else {
+        alert("Wait 1 seconde before next requests")
+    }
+});
+
+
+// ########## Gestion de l'interface et du changement de bdd #############
+
+// Sélectionne tous les éléments de la barre de navigation
+var navItems = document.querySelectorAll('.nav-href');
+
+// Retire la classe 'active' de tous les éléments enfants 'div'
+function removeActiveClass() {
+    navItems.forEach(function(nav) {
+        var childDiv = nav.querySelector('div');
+        if (childDiv) {
+            childDiv.classList.remove('active');
+        }
+    });
+}
+
+// Ajoute un écouteur d'événement 'click' à chaque élément
+navItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+        
+        // Ajoute la classe 'active' à l'enfant 'div' de l'élément cliqué
+        var clickedChildDiv = item.querySelector('div');
+        if (clickedChildDiv) {
+            if (window.location.href.split('#')[1] != item.href.split('#')[1] && item.classList[1] != 'no-load') {
+                removeActiveClass()
+
+                content.innerHTML = "";
+                clickedChildDiv.classList.add('active');
+                bddToLoad = item.href.split('#')[1];
+                startIndex = 0;
+            
+                start();
+                closeMenu();
+            }
+        }
+    });
+});
+
+var onFirstLoad = window.location.href.split('#')[1];
+
+if (onFirstLoad) {
+    bddToLoad = onFirstLoad;
+    removeActiveClass()
+    for (var i = 0; i < navItems.length; i++) {
+        if (navItems[i].href.split('#')[1] == onFirstLoad) {
+            navItems[i].querySelector('div').classList.add('active');
+            break;
+        }
+    }
+} else {
+    navItems[1].querySelector('div').classList.add('active');
+}
+
+start();
---
+++ js/main.js
@@ -0,0 +1,326 @@
+// ########### Check status client #########
+
+const botLeak = document.getElementById("botLeak");
+const statusBanner = document.getElementById("global-status");
+
+function isMoreThanTwoHoursOld(pythonDate) {
+    const now = new Date();
+    const pythonTime = new Date(pythonDate);
+
+    const diffInHours = (now - pythonTime) / (1000 * 60 * 60);
+
+    return diffInHours > 2;
+}
+
+fetch(`status.json`)
+    .then((response) => response.json())
+    .then((responseData) => {
+        const botLeakStatus = isMoreThanTwoHoursOld(responseData.botLeakStatus);
+
+        if (!botLeakStatus) {
+            botLeak.textContent = "opérationnel";
+        } else {
+            botLeak.textContent = "inopérationnel";
+            botLeak.classList.remove("operational")
+            botLeak.classList.add("inoperational")
+
+            statusBanner.style.backgroundColor = "red";
+            statusBanner.querySelector("h2").textContent = "Nous avons un problème général sur nos systèmes";
+        }
+        
+    })
+    .catch((error) =>
+        console.error("Erreur lors de la recherche:", error)
+    );
+
+
+// ########### Copy to clipboard #########
+
+const messageTemp = document.querySelector(".message-temp");
+const cooldownProgress = document.querySelector(".message-cooldown-progress");
+const messageContent = document.querySelector(".message-temp-content");
+
+var hasBeenRemoved = true;
+
+// Function to show the message
+function showMessage(txt) {
+    if (hasBeenRemoved) {
+        hasBeenRemoved = false;
+        console.log(txt)
+        messageContent.innerHTML = txt;
+        messageTemp.classList.add("show");
+        messageTemp.classList.remove("hide");
+
+        // Start the cooldown animation
+        cooldownProgress.style.transform = "scaleX(0)";
+
+        // Hide the message after a delay
+        setTimeout(hideMessage, 3000); // Adjustable duration
+    }
+}
+
+// Function to hide the message
+function hideMessage() {
+    if (!hasBeenRemoved) {
+        messageTemp.classList.add("hide");
+        messageTemp.classList.remove("show");
+
+        // Désactiver la transition, réinitialiser la barre instantanément
+        
+        setTimeout(() => {
+            cooldownProgress.style.transition = "none"; // Désactiver la transition
+            cooldownProgress.style.transform = "scaleX(1)";
+        }, 100);
+
+        // Réactiver la transition après un court délai
+        setTimeout(() => {
+            cooldownProgress.style.transition = "transform 3s linear"; // Réactiver avec la durée souhaitée
+            hasBeenRemoved = true;
+        }, 120);
+    }
+}
+
+
+function copy(copyThis) {
+    var copyText = document.getElementById(copyThis);
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+    
+    navigator.clipboard.writeText(copyText.value);
+
+    showMessage("Texte copié !");
+}
+
+
+// ########### linkvertise #########
+
+function transformToPaidLink(originalUrl, apiUrl, apiToken, advertType = 1) {
+    // Helper function to encode URL in Base64 format
+    function fps_b64_encode(url) {
+        return btoa(encodeURIComponent(url).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+            return String.fromCharCode('0x' + p1);
+        }));
+    }
+
+    if (!originalUrl || !apiUrl || !apiToken) {
+        console.error("Missing parameters: originalUrl, apiUrl, or apiToken");
+        return originalUrl;
+    }
+
+    // Construct the paid URL
+    return `${apiUrl}full?api=${encodeURIComponent(apiToken)}&url=${fps_b64_encode(originalUrl)}&type=${encodeURIComponent(advertType)}`;
+}
+
+// Example usage:
+const originalUrl = "https://example.com";
+const apiUrl = "https://shrtfly.com/";
+const apiToken = "a1e3a1bbddfc42bbdfd0eb2a600495bb";
+const advertType = 1; // Optional, default is 1
+
+// const paidLink = transformToPaidLink(originalUrl, apiUrl, apiToken, advertType);
+// console.log(paidLink); // This will print the transformed paid link
+
+
+// ########### menu mobile #########
+
+const menu_mobile = document.querySelector(".menu-mobile");
+const menu_input = document.getElementById("check");
+const nav = document.querySelector(".nav");
+const pc_translate = document.querySelector(".pc-translate");
+
+window.onload = (e) => {
+    if (window.innerWidth < 600) {
+        pc_translate.remove()
+        googleTranslateElementInit()
+    } else {
+        googleTranslateElementInit()
+    }
+}
+
+menu_mobile.addEventListener("click", () => {
+    if (menu_input.checked == true) {
+        closeMenu()
+    } else {
+        menu_input.checked = true;
+        nav.style.left = "0"
+    }
+})
+
+function closeMenu() {
+    if (window.innerWidth < 600) {
+        menu_input.checked = false;
+        nav.style.left = "-200px"
+    } 
+}
+
+// ########### report client #########
+
+function reportEvent(thisDiv) {
+    // Vérifie si l'élément existe déjà pour éviter de le recréer
+    if (document.querySelector('.report-box-temp')) return;
+
+    const imageToReport = thisDiv.parentElement.querySelector(".content-img").src;
+    const nameToReport = thisDiv.parentElement.querySelector("h3");
+
+    // Création de la structure principale
+    const reportContainer = document.createElement('div');
+    reportContainer.className = 'report-box-temp';
+    reportContainer.style.opacity = '0'; // Commence invisible
+    reportContainer.style.pointerEvents = 'none'; // Désactiver les interactions initialement
+    reportContainer.style.transition = 'background-color 0.5s ease, opacity 0.5s ease';
+
+    // Création du contenu interne
+    const reportContent = `
+        <div class="report-temp">
+            <img src="${imageToReport}" alt="">
+            <div class="report-temp-info">
+                <h2>${nameToReport.textContent}</h2>
+                <textarea type="text" placeholder="Raison du signalement (min 20 charactères, max 300 charactères)"></textarea>
+                <p class="countCharacter" style="color:red;">0/300<p>
+                <a href="#leak">Signaler</a>
+            </div>
+            <p class="close-button-report-temp">X</p>
+        </div>
+    `;
+
+    // Ajout du contenu HTML à la boîte principale
+    reportContainer.innerHTML = reportContent;
+
+    // Ajout au DOM (ajouté à la fin du body)
+    document.body.appendChild(reportContainer);
+
+    // Activer l'affichage avec une légère pause pour la transition
+    setTimeout(() => {
+        reportContainer.classList.add('active');
+        reportContainer.style.opacity = '1';
+        reportContainer.style.pointerEvents = 'all';
+    }, 10);
+
+    // Gestion du bouton pour fermer la boîte
+    const closeButton = reportContainer.querySelector('.close-button-report-temp');
+    closeButton.addEventListener('click', () => {
+        closeReportBox(reportContainer);
+    });
+
+    // Gestion du bouton pour fermer la boîte
+    // const report_box = reportContainer.parentElement.querySelector('.report-box-temp');
+    // report_box.addEventListener('click', () => {
+    //     closeReportBox(reportContainer);
+    // });
+
+    const textarea = reportContainer.querySelector('textarea');
+    const counter = reportContainer.querySelector('.countCharacter');
+    const maxChars = 300;
+    textarea.addEventListener('input', () => {
+        const textLength = textarea.value.length;
+
+        // Empêche d'écrire au-delà de la limite
+        if (textLength > maxChars) {
+            textarea.value = textarea.value.slice(0, maxChars);
+        }
+
+        // Met à jour le compteur
+        counter.textContent = `${textarea.value.length} / ${maxChars}`;
+
+        // Mettre le compteur en rouge si moins de 20 caractères
+        if (textLength < 20) {
+            counter.style.color = 'red';
+        } else {
+            counter.style.color = 'grey'; // Retour à la couleur normale
+        }
+    });
+
+
+    const button = reportContainer.querySelector('a')
+    button.addEventListener('click', () => {
+        const textToReport = reportContainer.querySelector('textarea').value;
+
+        const dataToSend = {
+            image: imageToReport,
+            name: nameToReport.textContent,
+            reason: textarea.value
+        };
+
+        if (textToReport.length <= 20) {
+            console.log("Texte trop court")
+        } else if (textToReport.length >= 300) {
+            console.log("Texte trop long")
+        } else {
+            fetch(`http://localhost:5000/report`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dataToSend)
+            })
+                .then((response) => response.json())
+                .then((responseData) => {
+                    showMessage(responseData.message)
+                    closeReportBox(reportContainer)
+                })
+                .catch((error) => console.error("Erreur:", error));
+        }
+    })
+}
+
+
+function closeReportBox(reportContainer) {
+    reportContainer.style.opacity = '0';
+    reportContainer.style.pointerEvents = 'none';
+    setTimeout(() => {
+        reportContainer.remove();
+    }, 500); // Attendre la fin de la transition avant de supprimer l'élément
+}
+
+// ########### style page #########
+
+// Sélectionne tous les éléments de la barre de navigation
+var navItems = document.querySelectorAll('.nav-href');
+
+// Ajoute un écouteur d'événement 'click' à chaque élément
+navItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+        // Retire la classe 'active' de tous les éléments enfants 'div'
+        navItems.forEach(function(nav) {
+            var childDiv = nav.querySelector('div');
+            if (childDiv) {
+                childDiv.classList.remove('active');
+            }
+        });
+        // Ajoute la classe 'active' à l'enfant 'div' de l'élément cliqué
+        var clickedChildDiv = item.querySelector('div');
+        if (clickedChildDiv) {
+            clickedChildDiv.classList.add('active');
+        }
+    });
+});
+
+
+// ########### translate #########
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'fr',
+        includedLanguages: 'en,fr,es,it,nl,pl,pt,tr',
+    }, 'google_translate_element');
+    
+    setTimeout(function() {
+        // Set the default language to Spanish
+        var selectElement = document.querySelector('#google_translate_element select');
+        var el = document.querySelector("#google_translate_element div"), child = el.firstChild, nextChild;
+
+        while (child) {
+            nextChild = child.nextSibling;
+            if (child.nodeType == 3) {
+                el.removeChild(child);
+                document.querySelector("#google_translate_element div span").remove();
+            }
+            child = nextChild;
+        }
+
+        // selectElement.value = 'fr';
+        // selectElement.dispatchEvent(new Event('change'));
+    }, 100);
+}
